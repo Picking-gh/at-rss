c6a9eee397575fc00ee67c9df484ec34ed53b745
picking <wangke@gddky.csg.cn>
2024-08-18 21:32:51 +0800

    modify for clearness

--- cache.go
+++ cache.go
@@ -16,22 +16,19 @@ import (
 	"time"
 )
 
-const cachePath = ".cache/at-rss.gob"
+const cacheFileName = ".cache/at-rss.gob"
 
-// Cache is a struct that holds information related to RSS feed items.
-// The `data` field is a map where each key is a feed URL (string), and the value
-// is another map that stores the GUIDs (Globally Unique Identifiers) of all the
-// items in that particular feed. Additionally, this map includes an "infoHash" key,
-// whose value is a map where the key is the btih (info hash) and the value is the
-// Unix timestamp indicating when the btih was added. The `path` field is a string
-// that specifies the file path where the cache data may be stored or retrieved from.
+// Cache manages the storage and retrieval of RSS feed items.
+// The `data` map contains feed URLs as keys, each associated with a map of GUIDs (Globally Unique Identifiers) and their UNIX timestamps.
+// Additionally, there is a special "infoHash" key with a map that tracks the btih (info hash) and its addition time.
+// The `filePath` stores the location for saving or loading the cache data.
 type Cache struct {
-	mu   sync.RWMutex
-	data map[string]map[string]int64 //inner map value is UNIX tiamstamp
-	path string
+	mu       sync.RWMutex
+	data     map[string]map[string]int64 // inner map value is a UNIX timestamp
+	filePath string
 }
 
-// NewCache creates a new Cache object.
+// NewCache initializes and returns a Cache instance.
 func NewCache(ctx context.Context) (*Cache, error) {
 	cache := &Cache{
 		data: make(map[string]map[string]int64),
@@ -46,31 +43,31 @@ func NewCache(ctx context.Context) (*Cache, error) {
 		slog.Error("Failed to locate user's home directory.", "err", err)
 		return nil, err
 	}
-	cache.path = filepath.Join(homeDir, cachePath)
+	cache.filePath = filepath.Join(homeDir, cacheFileName)
 
-	if err := readGob(cache.path, &cache.data); err != nil {
-		slog.Warn("Failed to read cache, initializing empty cache.", "err", err)
+	if err := loadCache(cache.filePath, &cache.data); err != nil {
+		slog.Warn("Failed to load cache, initializing empty cache.", "err", err)
 	}
 
 	return cache, nil
 }
 
-// Get returns a copy of the value associated with the key or returns an empty map if the key doesn't exist.
+// Get returns a copy of the map associated with the given key or an empty map if the key doesn't exist.
 func (c *Cache) Get(key string) map[string]int64 {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
-	if value, ok := c.data[key]; ok {
-		dest := make(map[string]int64)
+	if value, exists := c.data[key]; exists {
+		copiedValue := make(map[string]int64)
 		for k, v := range value {
-			dest[k] = v
+			copiedValue[k] = v
 		}
-		return dest
+		return copiedValue
 	}
 	return make(map[string]int64)
 }
 
-// Set stores the given value with the associated key in the cache and persists it.
+// Set stores the provided map under the specified key in the cache and timestamps the entries.
 func (c *Cache) Set(key string, value map[string]int64) {
 	if len(value) == 0 {
 		return
@@ -81,81 +78,60 @@ func (c *Cache) Set(key string, value map[string]int64) {
 	if _, exists := c.data[key]; !exists {
 		c.data[key] = make(map[string]int64)
 	}
-	data := c.data[key]
-	for key := range value {
-		data[key] = time.Now().Unix()
+	for k := range value {
+		c.data[key][k] = time.Now().Unix()
 	}
 }
 
-// RemoveNotIn removes entries from the cache that are not present in the provided value map.
-// It operates on the cache data corresponding to the given key. If the value map is empty, the function returns immediately.
-// The function locks the cache for thread-safe access, retrieves the map associated with the specified key,
-// and deletes any entries from that map which are not present in the provided value map.
-//
-// Parameters:
-//   - key: The key used to access the cache map for a specific sub map, usually feed URL.
-//   - value: A map where keys represent the valid entries. Any entry in the cache map with a key not present in this map will be removed.
-func (c *Cache) RemoveNotIn(key string, value map[string]int64) {
-	if len(value) == 0 {
+// RemoveNotIn deletes entries from the cache that are not present in the provided map.
+// This function operates on the cache map associated with the specified key, usually a feed URL.
+func (c *Cache) RemoveNotIn(key string, validEntries map[string]int64) {
+	if len(validEntries) == 0 {
 		return
 	}
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	subMap := c.data[key]
-	// Remove cache items that are not in the provided value map
-	for key := range subMap {
-		if _, found := value[key]; !found {
-			delete(subMap, key)
+	cacheSubMap := c.data[key]
+	for k := range cacheSubMap {
+		if _, exists := validEntries[k]; !exists {
+			delete(cacheSubMap, k)
 		}
 	}
 }
 
-// Flush serializes the cache data and writes it to disk at the specified path.
-// It locks the cache for thread-safe operations and uses the writeGob function
-// to perform the actual writing. It returns an error if the write operation fails.
+// Flush serializes the cache data and writes it to disk at the specified file path.
 func (c *Cache) Flush() error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	return writeGob(c.path, c.data)
+	return saveCache(c.filePath, c.data)
 }
 
-// cleanupExpiredEntries removes entries from the cache that have timestamps older than 24 hours.
-// It operates on the cache data corresponding to the specified key. If the key does not exist in the cache,
-// the function returns immediately. It locks the cache for thread-safe access, retrieves the map associated
-// with the specified key, and deletes entries that are older than one day.
+// cleanupExpiredEntries removes entries from the cache that are older than 24 hours.
 func (c *Cache) cleanupExpiredEntries(key string) {
-	oneDayAgo := time.Now().Add(-24 * time.Hour).Unix()
+	expirationTime := time.Now().Add(-24 * time.Hour).Unix()
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	// Access the cache for the specific key
-	subMap, exists := c.data[key]
-	if !exists {
-		return
-	}
-
-	// Iterate and delete expired entries
-	for key, timestamp := range subMap {
-		if timestamp < oneDayAgo {
-			delete(subMap, key)
+	if cacheSubMap, exists := c.data[key]; exists {
+		for k, timestamp := range cacheSubMap {
+			if timestamp < expirationTime {
+				delete(cacheSubMap, k)
+			}
 		}
 	}
 }
 
-// startCleanupScheduler initiates a scheduled cleanup task that runs every hour.
-// It takes a context for cancellation and a key to specify which cache map to clean.
-// The function uses a ticker to trigger the cleanup function every hour. It listens for context cancellation
-// and stops the cleanup task when the context is cancelled.
+// startCleanupScheduler initiates a cleanup task that runs every hour to remove expired entries.
+// The function stops when the context is cancelled.
 func (c *Cache) startCleanupScheduler(ctx context.Context, key string) {
-	ticker := time.NewTicker(time.Hour) // Trigger cleanup every hour
+	ticker := time.NewTicker(time.Hour)
 	defer ticker.Stop()
 
 	for {
 		select {
 		case <-ctx.Done():
-			// Exit when context is cancelled
 			return
 		case <-ticker.C:
 			c.cleanupExpiredEntries(key)
@@ -163,11 +139,9 @@ func (c *Cache) startCleanupScheduler(ctx context.Context, key string) {
 	}
 }
 
-// writeGob creates the necessary directories and serializes the given object
-// to a file using the gob encoding format. It ensures that the directory structure
-// exists before attempting to create the file. Returns an error if directory creation
-// or file writing fails.
-func writeGob(filePath string, object interface{}) error {
+// saveCache creates necessary directories and serializes the given object to a file using gob encoding.
+// It returns an error if directory creation or file writing fails.
+func saveCache(filePath string, object interface{}) error {
 	if err := os.MkdirAll(filepath.Dir(filePath), 0744); err != nil {
 		slog.Warn("Failed to create directory for cache file.", "err", err)
 		return err
@@ -183,10 +157,9 @@ func writeGob(filePath string, object interface{}) error {
 	return gob.NewEncoder(file).Encode(object)
 }
 
-// readGob opens a file and deserializes its contents into the provided object
-// using the gob encoding format. Returns an error if the file cannot be opened
-// or if decoding fails.
-func readGob(filePath string, object interface{}) error {
+// loadCache opens a file and deserializes its contents into the provided object using gob encoding.
+// Returns an error if the file cannot be opened or if decoding fails.
+func loadCache(filePath string, object interface{}) error {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
